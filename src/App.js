31ec59e43ddf5abd5dbc0b996514edd2c53861c4
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-04-11 07:34:04 +0100

    app file commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import NavBar from "./Components/NavBar.js";
 import NewTextArea from "./Components/NewTextArea.js";
 import PreviousNotes from "./Components/PreviousNotes.js";
 import './index.css';
 
-function App() {
+function App(props) {
+
+  
+  useEffect(() => {
+    document.title = 'Notes';
+  }, []);
+
   return (
     <div className="container">
       <div className="leftmost--container">
